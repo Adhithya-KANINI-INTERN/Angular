8e32988f20800e47a07eb2abf1d22c3164362d2b
Adhithya-KANINI-INTERN <adhithyababu.kanini@outlook.com>
2024-11-25 11:24:06 +0530

    rxjs concepts

--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -1,67 +1,6 @@
-// import { Component } from '@angular/core';
-// import { fromEvent, map, switchMap, of, Subscription } from 'rxjs';
-// import { catchError } from 'rxjs/operators';
-// import { ServiceService } from '../service.service';
-// import { CommonModule } from '@angular/common';
-
-// @Component({
-//   selector: 'app-rxjs',
-//   standalone: true,
-//   imports: [CommonModule],
-//   templateUrl: './rxjs.component.html',
-//   styleUrls: ['./rxjs.component.css'],
-// })
-// export class RxjsComponent {
-//   users: any[] = []; 
-//   private subscription!: Subscription;
-
-//   constructor(private service: ServiceService) {}
-
-//   ngOnInit() {
-//     const input = document.querySelector('input') as HTMLInputElement;
-
-//     if (!input) {
-//       console.error('Input element not found!');
-//       return;
-//     }
-
-//     this.subscription = fromEvent(input, 'input')
-//       .pipe(
-//         map((event: any) => event.target.value.trim()), 
-//         switchMap((value: string) => {
-//           const limit = Number(value);
-//           if (isNaN(limit) || limit <= 0) {
-//             return of([]);
-//           }
-//           return this.service.getusers(limit).pipe(
-//             catchError((err) => {
-//               console.error('Error fetching users:', err);
-//               return of([]); 
-//             })
-//           );
-//         })
-//       )
-//       .subscribe({
-//         next: (response) => {
-//           this.users = response; 
-//           console.log('Fetched users:', this.users);
-//         },
-//         error: (err) => {
-//           console.error('Error in subscription:', err);
-//         },
-//       });
-//   }
-
-//   ngOnDestroy() {
-//     if (this.subscription) {
-//       this.subscription.unsubscribe();
-//     }
-//   }
-// }
-
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Subscription } from 'rxjs';
+import { map, Subscription, switchMap } from 'rxjs';
 import { ServiceService } from '../service.service';
 import { FormsModule } from '@angular/forms';
 
@@ -87,7 +26,18 @@ export class RxjsComponent {
 
     console.log('Fetching users with limit:', this.limit); 
 
-    this.subscription = this.service.getusers(this.limit).subscribe({
+    this.subscription = this.service.getusers(this.limit).pipe(
+      map((response: any) => {
+        return response.map((user: any) => ({
+          ...user,
+          name: user.name.toUpperCase(),
+        }));
+      }),
+      switchMap((transformedUsers: any[]) => {
+        return [transformedUsers];
+      })
+    )
+    .subscribe({
       next: (response) => {
         console.log('API response:', response);
         this.users = response; 
